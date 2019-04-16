3d1c000c90c8c0c7d6ff9fa12a14180f6acba977
OLINY Damien <dodevup@gmail.com>
2019-04-16 15:23:49 -0400

    feat : user can wrap a table with overflow-x

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,170 +4,171 @@ const path = require('path');
 const swag = require('@ephox/swag');
 
 module.exports = function(grunt) {
-  var packageData = grunt.file.readJSON('package.json');
-  var BUILD_VERSION = packageData.version + '-' + (process.env.BUILD_NUMBER ? process.env.BUILD_NUMBER : '0');
-  const libPluginPath = 'lib/main/ts/Plugin.js';
-  const scratchPluginPath = 'scratch/compiled/plugin.js';
-  const scratchPluginMinPath = 'scratch/compiled/plugin.min.js';
-  const tsDemoSourceFile = path.resolve('src/demo/ts/Demo.ts');
-  const jsDemoDestFile = path.resolve('scratch/compiled/demo.js');
+    var packageData = grunt.file.readJSON('package.json');
+    var BUILD_VERSION = packageData.version + '-' + (process.env.BUILD_NUMBER ? process.env.BUILD_NUMBER : '0');
+    const libPluginPath = 'lib/main/ts/Plugin.js';
+    const scratchPluginPath = 'scratch/compiled/plugin.js';
+    const scratchPluginMinPath = 'scratch/compiled/plugin.min.js';
+    const tsDemoSourceFile = path.resolve('src/demo/ts/Demo.ts');
+    const jsDemoDestFile = path.resolve('scratch/compiled/demo.js');
 
-  grunt.initConfig({
-    pkg: packageData,
+    grunt.initConfig({
+        pkg: packageData,
 
-    clean: {
-      dirs: [ 'dist', 'scratch' ]
-    },
-
-    tslint: {
-      options: {
-        configuration: 'tslint.json'
-      },
-      plugin: ['src/**/*.ts']
-    },
-
-    shell: {
-      command: 'tsc'
-    },
-
-    rollup: {
-      options: {
-        treeshake: true,
-        name: 'plugin',
-        format: 'iife',
-        banner: '(function () {',
-        footer: 'plugin();})();',
-        onwarn: swag.onwarn,
-        plugins: [
-          swag.nodeResolve({
-            basedir: __dirname,
-            prefixes: {}
-          }),
-          swag.remapImports()
-        ]
-      },
-      plugin: {
-        files: [
-          {
-            src: libPluginPath,
-            dest: scratchPluginPath
-          }
-        ]
-      }
-    },
-
-    uglify: {
-      plugin: {
-        files: [
-          {
-            src: scratchPluginPath,
-            dest: scratchPluginMinPath
-          }
-        ]
-      }
-    },
-
-    concat: {
-      license: {
-        options: {
-          process: function(src) {
-            var buildSuffix = process.env.BUILD_NUMBER
-              ? '-' + process.env.BUILD_NUMBER
-              : '';
-            return src.replace(
-              /@BUILD_NUMBER@/g,
-              packageData.version + buildSuffix
-            );
-          }
-        },
-        // scratchPluginMinPath is used twice on purpose, all outputs will be minified for premium plugins
-        files: {
-          'dist/separator/plugin.js': [
-            'src/text/license-header.js',
-            scratchPluginMinPath
-          ],
-          'dist/separator/plugin.min.js': [
-            'src/text/license-header.js',
-            scratchPluginMinPath
-          ]
-        }
-      }
-    },
-
-    copy: {
-      css: {
-        files: [
-          {
-            cwd: 'src/text',
-            src: ['license.txt'],
-            dest: 'dist/separator',
-            expand: true
-          },
-          { src: ['changelog.txt'], dest: 'dist/separator', expand: true }
-        ]
-      }
-    },
-
-    webpack: {
-      options: {
-        mode: 'development',
-        watch: true
-      },
-      dev: {
-        entry: tsDemoSourceFile,
-        devtool: 'source-map',
-
-        resolve: {
-          extensions: ['.ts', '.js']
-        },
-
-        module: {
-          rules: [
-            {
-              test: /\.js$/,
-              use: ['source-map-loader'],
-              enforce: 'pre'
-            },
-            {
-              test: /\.ts$/,
-              use: [
-                {
-                  loader: 'ts-loader',
-                  options: {
-                    transpileOnly: true,
-                    experimentalWatchApi: true
-                  }
-                }
-              ]
+        clean: {
+            dirs: ["../../pwa/src/assets/tinymce/plugins/separator/", 'scratch'],
+            options: {
+                force: true
             }
-          ]
         },
 
-        plugins: [new LiveReloadPlugin(), new CheckerPlugin()],
+        tslint: {
+            options: {
+                configuration: 'tslint.json'
+            },
+            plugin: ['src/**/*.ts']
+        },
 
-        output: {
-          filename: path.basename(jsDemoDestFile),
-          path: path.dirname(jsDemoDestFile)
+        shell: {
+            command: 'tsc'
+        },
+
+        rollup: {
+            options: {
+                treeshake: true,
+                name: 'plugin',
+                format: 'iife',
+                banner: '(function () {',
+                footer: 'plugin();})();',
+                onwarn: swag.onwarn,
+                plugins: [
+                    swag.nodeResolve({
+                        basedir: __dirname,
+                        prefixes: {}
+                    }),
+                    swag.remapImports()
+                ]
+            },
+            plugin: {
+                files: [{
+                    src: libPluginPath,
+                    dest: scratchPluginPath
+                }]
+            }
+        },
+
+        uglify: {
+            plugin: {
+                files: [{
+                    src: scratchPluginPath,
+                    dest: scratchPluginMinPath
+                }]
+            }
+        },
+
+        concat: {
+            license: {
+                options: {
+                    process: function(src) {
+                        var buildSuffix = process.env.BUILD_NUMBER ?
+                            '-' + process.env.BUILD_NUMBER :
+                            '';
+                        return src.replace(
+                            /@BUILD_NUMBER@/g,
+                            packageData.version + buildSuffix
+                        );
+                    }
+                },
+                // scratchPluginMinPath is used twice on purpose, all outputs will be minified for premium plugins
+                files: {
+                    '../../pwa/src/assets/tinymce/plugins/separator/plugin.js': [
+                        'src/text/license-header.js',
+                        scratchPluginMinPath
+                    ],
+                    '../../pwa/src/assets/tinymce/plugins/separator/plugin.min.js': [
+                        'src/text/license-header.js',
+                        scratchPluginMinPath
+                    ]
+                }
+            }
+        },
+
+        copy: {
+            css: {
+                files: [{
+                        cwd: 'src/text',
+                        src: ['license.txt'],
+                        dest: '../../pwa/src/assets/tinymce/plugins/separator',
+                        expand: true
+                    },
+                    { src: ['changelog.txt'], dest: '../../pwa/src/assets/tinymce/plugins/separator', expand: true },
+                    {
+                        cwd: 'src/assets', // set working folder / root to copy
+                        src: '**/*', // copy all files and subfolders
+                        dest: '../../pwa/src/assets/tinymce/plugins/separator/assets', // destination folder
+                        expand: true // required when using cwd
+                    }
+                ]
+            }
+        },
+
+        webpack: {
+            options: {
+                mode: 'development',
+                watch: true
+            },
+            dev: {
+                entry: tsDemoSourceFile,
+                devtool: 'source-map',
+
+                resolve: {
+                    extensions: ['.ts', '.js']
+                },
+
+                module: {
+                    rules: [{
+                            test: /\.js$/,
+                            use: ['source-map-loader'],
+                            enforce: 'pre'
+                        },
+                        {
+                            test: /\.ts$/,
+                            use: [{
+                                loader: 'ts-loader',
+                                options: {
+                                    transpileOnly: true,
+                                    experimentalWatchApi: true
+                                }
+                            }]
+                        }
+                    ]
+                },
+
+                plugins: [new LiveReloadPlugin(), new CheckerPlugin()],
+
+                output: {
+                    filename: path.basename(jsDemoDestFile),
+                    path: path.dirname(jsDemoDestFile)
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  require('load-grunt-tasks')(grunt);
-  grunt.loadNpmTasks('@ephox/swag');
+    require('load-grunt-tasks')(grunt);
+    grunt.loadNpmTasks('@ephox/swag');
 
-  grunt.registerTask('version', 'Creates a version file', function () {
-    grunt.file.write('dist/separator/version.txt', BUILD_VERSION);
-  });
+    grunt.registerTask('version', 'Creates a version file', function() {
+        grunt.file.write('../../pwa/src/assets/tinymce/plugins/separator/version.txt', BUILD_VERSION);
+    });
 
-  grunt.registerTask('default', [
-    'clean',
-    'tslint',
-    'shell',
-    'rollup',
-    'uglify',
-    'concat',
-    'copy',
-    'version'
-  ]);
+    grunt.registerTask('default', [
+        'clean',
+        'tslint',
+        'shell',
+        'rollup',
+        'uglify',
+        'concat',
+        'copy',
+        'version'
+    ]);
 };
--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -1,17 +1,351 @@
+import { document } from "@ephox/dom-globals";
+import { Editor } from "tinymce";
+
 declare const tinymce: any;
 
-const setup = (editor, url) => {
-  editor.addButton('separator', {
-    text: 'separator button',
+const setup = (editor: Editor, url) => {
+  editor.addButton("separator", {
+    image: tinymce.baseURL + "/plugins/separator/assets/icons/separator.png",
     icon: false,
     onclick: () => {
-      // tslint:disable-next-line:no-console
-      editor.setContent('<p>content added from separator</p>');
+      editor.execCommand(
+        "mceInsertContent",
+        false,
+        `<p style="line-height:1px; width: 100%; border-top:1px solid #636363; margin:0; padding: 0;"></p><p></p>`
+      );
+      editor.insertContent(`<p></p>`);
     }
   });
+
+  const toWrappedContent = function(element) {
+    if (element.nodeName.toLowerCase() === "body") {
+      const parent = element;
+      const wrapper = parent.createElement("div");
+      wrapper.appendChild(element);
+      return wrapper;
+    } else {
+      const parent = element.parentNode;
+      const wrapper = document.createElement("div");
+
+      // set the wrapper as child (instead of the element)
+      parent.replaceChild(wrapper, element);
+      // set element as child of wrapper
+      wrapper.appendChild(element);
+      return wrapper;
+    }
+  };
+
+  const toUnwrappedContent = function(element, className) {
+    // get the element's parent node
+    const parent = findUpClass(element, className);
+    if (!parent) {
+      return null;
+    }
+    const granParent = parent.parentNode;
+
+    // move all children out of the element
+    while (parent.firstChild) {
+      granParent.insertBefore(parent.firstChild, parent);
+    }
+
+    // remove the empty element
+    granParent.removeChild(parent);
+
+    return granParent;
+  };
+
+  const wrapContent = function(selection, options?) {
+    const wrapper = toWrappedContent(selection);
+    wrapper.style.backgroundColor = "#f3f3f3";
+    wrapper.style.border = "1px solid #bbb";
+    wrapper.style.paddingLeft = "1rem";
+    wrapper.style.paddingRight = "1rem";
+    wrapper.style.borderRadius = "4px";
+    if (options) {
+      Object.assign(wrapper.style, options);
+    }
+    return wrapper;
+  };
+
+  const wrapTable = function(table, options?) {
+    const parent = table.parentNode;
+    console.log(parent);
+    const wrapper = document.createElement("div");
+    wrapper.style.width = "100%";
+    wrapper.style["overflow-x"] = "auto";
+    if (options) {
+      Object.assign(wrapper.style, options);
+    }
+
+    parent.replaceChild(wrapper, table);
+    // set element as child of wrapper
+    wrapper.appendChild(table);
+
+    return wrapper;
+  };
+
+  const findUpTag = function(el, tag) {
+    if (el.nodeName.toLowerCase() === tag.toLowerCase()) {
+      return el;
+    }
+    while (el.parentNode) {
+      el = el.parentNode;
+      if (el.nodeName.toLowerCase() === tag.toLowerCase()) {
+        return el;
+      }
+    }
+    return null;
+  };
+
+  const findUpClass = function(el, className) {
+    console.log(">1 el.classList", el.classList);
+    if (el.classList && el.classList.contains(className)) {
+      return el;
+    }
+    while (el.parentNode) {
+      el = el.parentNode;
+      console.log("> el.classList", el.classList);
+      if (el.classList && el.classList.contains(className)) {
+        return el;
+      }
+    }
+    return null;
+  };
+
+  editor.addButton("wrapper", {
+    type: "splitbutton",
+    icon: "insert-time",
+    tooltip: "Wrap element into container",
+    text: "Wrap",
+    classes: "wrap-content",
+    onclick: () => {
+      const selection =
+        (editor.selection && editor.selection.getNode()) || null;
+      if (!selection) {
+        return;
+      }
+
+      const table = findUpTag(selection, "table");
+      const container = findUpClass(selection, "editor-container");
+
+      if (container) {
+        toUnwrappedContent(selection, "editor-container");
+      } else if (table) {
+        const wrapper = wrapTable(table);
+        wrapper.classList.add("editor-container");
+      } else {
+        const wrapper = wrapContent(selection, {
+          backgroundColor: "#f3f3f3",
+          border: "1px solid #bbb"
+        });
+        wrapper.classList.add("editor-container");
+        wrapper.classList.add("editor-container-gray");
+      }
+    },
+    menu: [
+      {
+        text: "Gris",
+        classes: "wrap-content-gray",
+        onclick: function() {
+          const selection =
+            (editor.selection && editor.selection.getNode()) || null;
+          if (!selection) {
+            return;
+          }
+          const table = findUpTag(selection, "table");
+          const container = findUpClass(selection, "editor-container");
+          let wrapper;
+
+          if (container) {
+            toUnwrappedContent(selection, "editor-container");
+          }
+          if (table) {
+            wrapper = wrapTable(table, {
+              backgroundColor: "#f3f3f3"
+            });
+          } else {
+            wrapper = wrapContent(selection, {
+              backgroundColor: "#f3f3f3",
+              border: "1px solid #bbb"
+            });
+          }
+
+          if (wrapper) {
+            wrapper.classList.add("editor-container");
+            wrapper.classList.add("editor-container-gray");
+          }
+        }
+      },
+      {
+        text: "Bleu",
+        classes: "wrap-content-blue",
+        onclick: function() {
+          const selection =
+            (editor.selection && editor.selection.getNode()) || null;
+          if (!selection) {
+            return;
+          }
+          const table = findUpTag(selection, "table");
+          const container = findUpClass(selection, "editor-container");
+          let wrapper;
+
+          if (container) {
+            toUnwrappedContent(selection, "editor-container");
+          }
+          if (table) {
+            wrapper = wrapTable(table, {
+              backgroundColor: "#d9edf6"
+            });
+          } else {
+            wrapper = wrapContent(selection, {
+              backgroundColor: "#d9edf6",
+              border: "1px solid #beddeb"
+            });
+          }
+
+          if (wrapper) {
+            wrapper.classList.add("editor-container");
+            wrapper.classList.add("editor-container-blue");
+          }
+        }
+      },
+      {
+        text: "Vert",
+        classes: "wrap-content-green",
+        onclick: function() {
+          const selection =
+            (editor.selection && editor.selection.getNode()) || null;
+          if (!selection) {
+            return;
+          }
+          const table = findUpTag(selection, "table");
+          const container = findUpClass(selection, "editor-container");
+          let wrapper;
+
+          if (container) {
+            toUnwrappedContent(selection, "editor-container");
+          }
+          if (table) {
+            wrapper = wrapTable(table, {
+              backgroundColor: "#def0d8"
+            });
+          } else {
+            wrapper = wrapContent(selection, {
+              backgroundColor: "#def0d8",
+              border: "1px solid #b9d4b0"
+            });
+          }
+
+          if (wrapper) {
+            wrapper.classList.add("editor-container");
+            wrapper.classList.add("editor-container-green");
+          }
+        }
+      },
+      {
+        text: "Jaune",
+        classes: "wrap-content-yellow",
+        onclick: function() {
+          const selection =
+            (editor.selection && editor.selection.getNode()) || null;
+          if (!selection) {
+            return;
+          }
+          const table = findUpTag(selection, "table");
+          const container = findUpClass(selection, "editor-container");
+          let wrapper;
+
+          if (container) {
+            toUnwrappedContent(selection, "editor-container");
+          }
+          if (table) {
+            wrapper = wrapTable(table, {
+              backgroundColor: "#fdf8e4"
+            });
+          } else {
+            wrapper = wrapContent(selection, {
+              backgroundColor: "#fdf8e4",
+              border: "1px solid #f4edd1"
+            });
+          }
+
+          if (wrapper) {
+            wrapper.classList.add("editor-container");
+            wrapper.classList.add("editor-container-yellow");
+          }
+        }
+      }
+    ]
+  });
+
+  editor.addButton("citation", {
+    image: tinymce.baseURL + "/plugins/separator/assets/icons/quote.png",
+    icon: false,
+    tooltip: "Wrap element into quote",
+    classes: "wrap-citation",
+    onclick: () => {
+      const selection =
+        (editor.selection && editor.selection.getNode()) || null;
+      if (!selection) {
+        return;
+      }
+      const parentNode = findUpClass(selection, "editor-citation");
+
+      if (parentNode) {
+        toUnwrappedContent(selection, "editor-citation");
+      } else {
+        const wrapper = toWrappedContent(selection);
+        wrapper.style.borderLeft = "0.2rem solid #ccc";
+        wrapper.style.paddingLeft = "1rem";
+        wrapper.style.marginLeft = "1rem";
+        wrapper.style.marginTop = "0.5rem";
+        wrapper.style.marginBottom = "0.5rem";
+        wrapper.classList.add("editor-citation");
+        editor.insertContent(`<p></p>`);
+      }
+    }
+  });
+
+  const editorEventCallback = function(e) {
+    if (!e) {
+      return;
+    }
+    const element = e.element;
+    if (!element) {
+      return;
+    }
+
+    const parentNode = findUpClass(element, "editor-container");
+    if (parentNode) {
+      $(".mce-wrap-content").addClass("mce-active");
+    } else {
+      $(".mce-wrap-content").removeClass("mce-active");
+    }
+
+    const colors = ["gray", "blue", "green", "yellow"];
+    for (const color of colors) {
+      if (
+        parentNode &&
+        parentNode.classList.contains(`editor-container-${color}`)
+      ) {
+        $(`.mce-wrap-content-${color}`).addClass("mce-active");
+      } else {
+        $(`.mce-wrap-content-${color}`).removeClass("mce-active");
+      }
+    }
+
+    const pNode = findUpClass(element, "editor-citation");
+    if (pNode) {
+      $(".mce-wrap-citation").addClass("mce-active");
+    } else {
+      $(".mce-wrap-citation").removeClass("mce-active");
+    }
+  };
+  editor.on("NodeChange", editorEventCallback);
+  // editor.off("NodeChange", editorEventCallback);
 };
 
-tinymce.PluginManager.add('separator', setup);
+tinymce.PluginManager.add("separator", setup);
 
 // tslint:disable-next-line:no-empty
 export default () => {};
